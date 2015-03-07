966ddc5304357121a26c14d53f9aaa6bdcecbb8d
manolobiene <manel-360@outlook.com>
2015-03-07 03:24:52 +0100

    Extra commit

---
+++ Assets/Spinner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+	GameObject plane;
+	GeneralAcceleration GA;
+	bool spin = false;
+	public bool antiClockWise = false;
+	
+	void Start () {
+		plane = GameObject.FindGameObjectWithTag ("Player");
+		GA = plane.GetComponent<GeneralAcceleration> ();
+	}
+
+	void FixedUpdate () {
+		if (spin) {
+			if (GA.PaperMode == GeneralAcceleration.PaperModes.PaperPlane){
+				if (antiClockWise == false) {
+					plane.transform.rotation *= Quaternion.Euler (new Vector3 (0, 0, 1) * 2 * GA.TIME);
+				} else {
+					plane.transform.rotation *= Quaternion.Euler (new Vector3 (0, 0, -1) * 2 * GA.TIME);
+				}
+			}
+		}
+	}
+
+	void OnMouseEnter () {
+		spin = true;
+	}
+
+	void OnMouseExit () {
+		spin = false;
+	}
+}
---
+++ Assets/Toucher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toucher : MonoBehaviour {
+	public float lastTouch;
+	GameObject plane;
+
+	void Start () {
+		plane = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void OnMouseDown () {
+		lastTouch = 0;
+	}
+
+	void FixedUpdate () {
+		lastTouch += 1 * Time.deltaTime;
+		//Debug.Log (lastTouch);
+	}
+}
